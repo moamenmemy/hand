daff7282d93f5cb8d08b6d8998b4d5bba73090eb
moamen memy <memmoamen82@gmail.com>
2026-02-11 01:25:41 +0200

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HeroComponent } from './features/hero/hero.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ { path: '', component: HeroComponent }
+];
