563db1b0654d3884da74e8f60ca7fce50b484e4c
moamen memy <memmoamen82@gmail.com>
2026-02-11 23:52:48 +0200

    btn

--- src/app/peges/contact/contact.component.ts
+++ src/app/peges/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+router = inject(Router);
 
+  goBack(){
+    this.router.navigate(['/']);
+  }
 }
